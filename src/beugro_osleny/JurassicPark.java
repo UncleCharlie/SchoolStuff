1a5116202213430399e47d30baa8b098aa08696e
guthk <guthk@DESKTOP-1N900PK>
2018-12-10 14:01:45 +0100

    Egyetem complete

---
+++ src/beugro_osleny/JurassicPark.java
@@ -0,0 +1,26 @@
+package beugro_osleny;
+
+
+import java.util.ArrayList;
+import beugro_osleny.Oshullo;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author guthk
+ */
+public class JurassicPark {
+    private ArrayList<Oshullo> allomany = new ArrayList<Oshullo>();
+    
+    
+    
+    
+    
+    
+    
+}
